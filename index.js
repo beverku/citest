f4d89fded02de76554852c241ea26184a398ad82
rbever <rbever@netapp.com>
2015-12-01 16:08:02 -0700

    Add a url to cause an application exit

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.get('/', function(request, response) {
   response.send(result);
 })
 
+app.get('/crashLater', function(request, response) {
+	var exitValue = 1;
+	response.json(`I'm going to crash in 1s - with Exit Value: ${exitValue}`);
+	console.log(`I'm going to crash in 1s - with Exit Value: ${exitValue}`);
+	setTimeout(function() {
+		console.log("timeout up - crashing now");
+		process.exit(exitValue);
+	}, 1000);
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
